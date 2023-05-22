1e1c18da980ce0e33c9d82169b2b44982bd9301f
MIKOFI <zekofm1@gmail.com>
2023-05-23 01:13:03 +0200

    dodal router za loge

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/userRoutes');
 var boxRouter = require('./routes/boxRoutes');
+var logRouter = require('./routes/logRoutes');
 
 var app = express();
 
@@ -67,6 +68,7 @@ app.use(function (req, res, next) {
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/box',boxRouter);
+app.use('/log',logRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
