1cce4a68595a9aad562e3f6a69394608c11af601
LeonardoP01 <leonardo.puncec@student.um.si>
2023-05-23 19:13:41 +0200

    bugfix

--- frontend/src/components/Log.js
+++ frontend/src/components/Log.js
@@ -1,5 +1,21 @@
 import React, { useState } from 'react';
 
+function Log() {
+
+    const Log = () => {
+        const [log, setLog] = React.useState([]);
+
+        try {
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return (
+
+    );
+}
 
 
 export default Log;
