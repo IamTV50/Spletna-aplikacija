9c34da2e3b6b1d1dd0e945e02385c63cda49d99d
LeonardoP01 <leonardo.puncec@student.um.si>
2023-05-23 20:03:02 +0200

    Added logic for adding log to db when opening box (TEST DATA)

--- frontend/src/components/Logs.js
+++ frontend/src/components/Logs.js
@@ -1,3 +1,47 @@
 import React, { useState } from 'react';
 import Log from './Log';
 import Cookies from 'js-cookie';
+
+function Logs() {
+    const [logs, setLogs] = useState([]);
+    const user = Cookies.get('uporabnik');
+  
+    useEffect(() => {
+      const fetchLogs = async () => {
+        try {
+          const response = await fetch('http://localhost:3001/logs');
+          if (response.ok) {
+            const data = await response.json();
+            setLogs(data);
+          } else {
+            console.log('Failed to fetch logs');
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      };
+  
+      fetchLogs();
+    }, []);
+  
+    return (
+      <div>
+        <h3>Logs:</h3>
+        {logs.length > 0 ? (
+          <ul>
+            {logs.map((log) => (
+              <Log
+                key={log._id}
+                user={log.user}
+                opend={log.opend}
+              />
+            ))}
+          </ul>
+        ) : (
+          <p>No logs found.</p>
+        )}
+      </div>
+    );
+  }
+  
+  export default Logs;
